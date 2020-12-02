916b2f8519999f8ee753a8ab814a8a7e6891e60f
Umma Ahimsha <uahimsha@gmail.com>
2020-12-02 16:24:42 +0700

    added abstract class

--- src/CharCollection.ts
+++ src/CharCollection.ts
@@ -1,5 +1,9 @@
-export class CharCollection {
-  constructor(public data: string) {}
+import { Sorter } from "./Sorter";
+
+export class CharCollection extends Sorter {
+  constructor(public data: string) {
+    super();
+  }
 
   get length(): number {
     return this.data.length;
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import { Sorter } from "./Sorter";
-
 import { NumbersCollection } from "./NumbersCollection";
 import { CharCollection } from "./CharCollection";
 import { LinkedList } from "./LinkedList";
@@ -15,8 +13,7 @@ console.log("Number Sorting ");
 console.log("---------------");
 console.log("Before: ", numbersColl.data);
 
-const sortNum = new Sorter(numbersColl);
-sortNum.sort();
+numbersColl.sort();
 console.log("After: ", numbersColl.data);
 
 console.log("\n");
@@ -27,14 +24,12 @@ console.log("String Sorting ");
 console.log("---------------");
 console.log("Before: ", someString.data);
 
-const sortString = new Sorter(someString);
-sortString.sort();
+someString.sort();
 console.log("After: ", someString.data);
 
 console.log("\n");
 
-//
-// Linked list operation
+//  Linked list operation
 
 const someList = new LinkedList();
 someList.add(55);
@@ -46,11 +41,10 @@ someList.add(30);
 console.log("Linked list Sorting");
 console.log("-------------------");
 
-const sortList = new Sorter(someList);
 console.log("Before:");
 someList.print();
 
-sortList.sort();
+someList.sort();
 
 console.log("After:");
 someList.print();
